6e0ce6c417aa93680d0fee005b28c7fbc29c8a15
LucasGarcez <garcezlucas.silva@gmail.com>
2025-01-14 17:28:13 -0300

    feat: create register screen

---
+++ delivery-mobile/src/screens/register/Register.tsx
@@ -0,0 +1,43 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./Register.style";
+import Header from "../../components/header/Header";
+import TextBox from "../../components/textBox/TextBox";
+import Button from "../../components/button/Button";
+
+const Register = () => {
+  return (
+    <View style={styles.container}>
+      <Header text="Criar sua conta" />
+
+      <View style={styles.formGroup}>
+        <View style={styles.form}>
+          <TextBox label="Nome completo" />
+        </View>
+
+        <View style={styles.form}>
+          <TextBox label="E-mail" />
+        </View>
+
+        <View style={styles.form}>
+          <TextBox label="Escolha uma senha" isPassword={true} />
+        </View>
+
+        <View style={styles.form}>
+          <TextBox label="Confirme a senha" isPassword={true} />
+        </View>
+
+        <View style={styles.formButton}>
+          <Button title="Próximo passo" />
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <TouchableOpacity>
+          <Text style={styles.footerText}>Acessar minha conta</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Register;
